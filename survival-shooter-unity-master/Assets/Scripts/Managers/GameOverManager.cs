fc6f16a634a8cd1a458963252feebb2de7fde79b
Alfandavi Aryo Utomo <13519211@std.stei.itb.ac.id>
2022-04-06 15:32:09 +0700

    init file labpro

---
+++ survival-shooter-unity-master/Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour
+{
+    public Text warningText;
+    public PlayerHealth playerHealth;
+    public float restartDelay = 5f;
+
+    Animator anim;
+    float restartTimer;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance)
+    {
+        if (playerHealth.currentHealth > 0)
+        {
+            warningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+            anim.SetTrigger("Warning");
+
+        }
+    }
+}
---
+++ survival-shooter-unity-master/README.md
@@ -0,0 +1,11 @@
+# Survival Shooter | Unity Learn
+
+Berikut adalah template dari game Survival Shooter yang harus Anda kembangkan. Anda diperbolehkan untuk memodifikasi semua aspek dari program, termasuk struktur folder jika diperlukan. Jangan lupa untuk mengubah README sesuai kebutuhan Anda.
+Disclaimer: Program ini tidak sempurna, hati-hati terhadap bug yang mungkin terdapat pada program.
+
+![Screenshot](./Docs/Screenshot.jpg)
+
+## Credits
+
+- Unity Learn
+- Agate Academy
