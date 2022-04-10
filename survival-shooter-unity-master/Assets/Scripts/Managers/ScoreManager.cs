3d936b76bdcb315e5ad67e8ec98692f3b1b1d288
ravielze <ravielz20@gmail.com>
2022-04-10 14:51:12 +0700

    Add zenmode

--- survival-shooter-unity-master/Assets/Scripts/Managers/ScoreManager.cs
+++ survival-shooter-unity-master/Assets/Scripts/Managers/ScoreManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
 
 public class ScoreManager : MonoBehaviour
 {
     public static int score;
     public string gameMode = "Wave";
+
+    private static float time = 0;
     Text text;
 
     void Awake()
@@ -14,13 +17,32 @@ public class ScoreManager : MonoBehaviour
         score = 0;
     }
 
+    public static int TimeSurvived()
+    {
+        return time;
+    }
 
     void Update()
     {
+        time += Time.deltaTime;
         string appended = "";
         if (gameMode == "Wave")
         {
             appended = "Wave: " + EnemyManager.Wave();
+        }
+        else
+        {
+            int rt = (int)time;
+            int sec = rt % 60;
+            int min = (rt / 60);
+            if (min == 0)
+            {
+                appended = "Time Survived: " + $"{sec}s";
+            }
+            else
+            {
+                appended = "Time Survived: " + $"{min}m {sec}s";
+            }
         }
         text.text = "Score: " + score + " - " + appended;
     }
